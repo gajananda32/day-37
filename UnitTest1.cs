80a4dbc1c2d86d6434adaf16b72399008464c67e
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-26 11:46:53 +0530

    Added RestSharpTestCases

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,36 +1,86 @@
-using EmployeePayRoll;
-using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
 
-namespace EmployeePayRollTest
+namespace RestSharpTest
 {
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void Given10Employee_WhenAdded_toList_ShouldMatchEmployee_Entries()
+        RestClient client;
+        [TestInitialize]
+        public void Setup()
         {
-            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 1, employeeName: "Abhishek", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 2, employeeName: "Jhon", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 3, employeeName: "Peter", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 4, employeeName: "Tony", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 5, employeeName: "Stark", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 6, employeeName: "kent", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 7, employeeName: "joe", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 8, employeeName: "Roy", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 9, employeeName: "Chunnu", phonenumber: "7894561230"));
-            employeeDetails.Add(new EmployeeDetails(emloyeeId: 10, employeeName: "Munnu", phonenumber: "7894561230"));
-            EmployeePayRollOperation employeePayRollOperation = new EmployeePayRollOperation();
-            // employeePayRollOperation.addEmployeeToPayRoll(employeeDetails);
-            DateTime startDateTime = DateTime.Now;
-            employeePayRollOperation.addEmployeeToPayRoll(employeeDetails);
-            DateTime stopDateTime = DateTime.Now;
-            Console.WriteLine("Duration without thread: " + (stopDateTime - stopDateTime));
+            client = new RestClient(
+            "http://localhost:4000");
+        }
 
-            DateTime startDateTimeThread = DateTime.Now;
-            employeePayRollOperation.addEmloyeeToPayrollWithThread(employeeDetails);
-            DateTime stopDateTimeThread = DateTime.Now;
-            Console.WriteLine("Duration with thread: " + (stopDateTimeThread - stopDateTimeThread));
-        }                    }
+        [TestMethod]
+        public void OnlyCallingList_return_emloyeelist()
+        {
+            IRestResponse response = getEmloyeeList();
+            Assert.AreEqual(response.StatusCode,HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(2, dataResponse.Count);
+            foreach (Employee e in dataResponse)
+            {
+                Console.WriteLine("id: " + e.id + " name: " + e.Name + " salary: " + e.Salary);
+            }
+        }
+        private IRestResponse getEmloyeeList()
+        {
+            RestRequest request = new RestRequest("/emloyees", Method.GET);
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        [TestMethod]
+        public void givenEmployee_onpost_shold_returnEmployee()
+        {
+            RestRequest request = new RestRequest("/emlpoyees/2", Method.POST);
+            JObject jobjectbody = new JObject();
+            jobjectbody.Add("name", "Clark");
+            jobjectbody.Add("Salary", 5000);
+            request.AddParameter("application/json", jobjectbody, ParameterType.RequestBody);
+            IRestResponse response=client.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            Employee dataresponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataresponse.Name);
+            Assert.AreEqual(5000, dataresponse.Salary);
+            Console.WriteLine(response.Content);
+        }
+        [TestMethod]
+        public void givenEmployee_onUpadate_shold_returnEmployee()
+        {
+            RestRequest request = new RestRequest("/emlpoyees/2", Method.PUT);
+            JObject jobjectbody = new JObject();
+            jobjectbody.Add("name", "Clark");
+            jobjectbody.Add("Salary", 5000);
+            request.AddParameter("application/json", jobjectbody, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Employee dataresponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataresponse.Name);
+            Assert.AreEqual(5000, dataresponse.Salary);
+            Console.WriteLine(response.Content);
+        }
+        [TestMethod]
+        public void GevenEmloyeeID_OnDelete_ShouldReturnStatus()
+        {
+            RestRequest request = new RestRequest("/Emloyees/2", Method.DELETE);
+
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Console.WriteLine(response.Content);
+        }
+
     }
 }
